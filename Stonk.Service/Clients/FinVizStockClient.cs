ea10d1d96f7aa4c270e386581dfd69279d799f64
bculo <culobozo@gmail.com>
2021-03-14 20:11:07 +0100

    Finviz client refactored and comments added

--- Stonk.Service/Clients/FinVizStockClient.cs
+++ Stonk.Service/Clients/FinVizStockClient.cs
@@ -15,6 +15,9 @@ using System.Threading.Tasks;
 
 namespace Stonk.Service.Clients
 {
+    /// <summary>
+    /// Client for Finviz site
+    /// </summary>
     public class FinVizStockClient : HttpBaseClient, IStonkInfoClient
     {
         private readonly IHtmlLookupService _lookup;
@@ -28,36 +31,82 @@ namespace Stonk.Service.Clients
             _lookup = lookup;
         }
 
-
+        /// <summary>
+        /// Fetch stock information and data
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
         public async Task<Result<StockInfoClientResponseDto>> GetStockInfo(string symbol)
         {
+            _logger.LogInformation("Fetching data for ticker {0}", symbol);
+
             AddQueryParam("t", symbol);
 
             var response = await _http.GetAsync(BuildQuery());
+
+            _logger.LogInformation("Data fetched for ticker {0}", symbol);
 
             var result = await HandleClientResponse(response);
 
             if (!result.Succedded)
             {
+                _logger.LogInformation("Fetchind data for ticker {0} failed -> {1}", symbol, result.Message);
                 return ResultExtensions.Failure<StockInfoClientResponseDto>(result.Message);
             }
 
             if (!ContainsTickerId(result.Instance, symbol))
             {
+                _logger.LogInformation("Ticker {0} not found", symbol);
                 return ResultExtensions.Failure<StockInfoClientResponseDto>("Ticker not found", (int)HttpStatusCode.NotFound);
             }
 
             var fetchedInstance = ParseInstanceFromHtmlContent(result.Instance);
 
+            _logger.LogInformation("Data for ticker fetched and prepared");
+
             return ResultExtensions.Success(fetchedInstance);
         }
 
+        /// <summary>
+        /// Create StockInfoClientResponseDto instance using html content
+        /// </summary>
+        /// <param name="html"></param>
+        /// <returns></returns>
         private StockInfoClientResponseDto ParseInstanceFromHtmlContent(string html)
         {
             var dto = new StockInfoClientResponseDto { };
 
+            _logger.LogInformation("Filling response instance with data from HTML");
+
+            FillSourceInformation(dto);
+
+            FillBasicStockInfo(html, dto);
+
+            FillAnalyticData(html, dto);
+
+            _logger.LogInformation("Response instances filled");
+
+            return dto;
+        }
+
+        /// <summary>
+        /// Fill instnace with data source information
+        /// </summary>
+        /// <param name="dto"></param>
+        private void FillSourceInformation(StockInfoClientResponseDto dto)
+        {
             dto.PageSource = "finviz";
             dto.PageSourceUrl = _url.Url;
+        }
+
+        /// <summary>
+        /// Fill instance with stock symbol, stock name, country origin etc.
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="dto"></param>
+        private void FillBasicStockInfo(string html, StockInfoClientResponseDto dto)
+        {
+            _logger.LogInformation("Filling basic info about given ticker");
 
             dto.Symbol = _lookup.FindHtmlElementById(html, "ticker", true);
 
@@ -71,10 +120,24 @@ namespace Stonk.Service.Clients
             dto.SubSector = _lookup.FindHtmlElementByTag(sectorDetailsHtmlSection, "a", 1, true);
             dto.Country = _lookup.FindHtmlElementByTag(sectorDetailsHtmlSection, "a", 2, true);
 
+            dto.Description = _lookup.FindHtmlElementByClass(html, "fullview-profile", true);
+
+            _logger.LogInformation("Instance filled with basic info");
+        }
+
+        /// <summary>
+        /// Fill instance with analytic data like market price, prediction price, price movment and so on
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="dto"></param>
+        private void FillAnalyticData(string html, StockInfoClientResponseDto dto)
+        {
+            _logger.LogInformation("Filling instance with analytic data");
+
             var snapShotSection = _lookup.FindHtmlElementByClass(html, "snapshot-table2"); // table details for given ticker
 
             var firstRowSection = _lookup.FindHtmlElementByTag(snapShotSection, "tr", 0); // --> First table row
-            dto.InsiderOwn = _lookup.FindHtmlElementByTag(firstRowSection, "b", 3, true); 
+            dto.InsiderOwn = _lookup.FindHtmlElementByTag(firstRowSection, "b", 3, true);
             dto.SharesOutstand = _lookup.FindHtmlElementByTag(firstRowSection, "b", 4, true);
 
             var secondRowSection = _lookup.FindHtmlElementByTag(snapShotSection, "tr", 1); // --> Second table row
@@ -104,11 +167,15 @@ namespace Stonk.Service.Clients
             var rowElevenSection = _lookup.FindHtmlElementByTag(snapShotSection, "tr", 11); // --> 11. table row
             dto.Volume = _lookup.FindHtmlElementByTag(rowElevenSection, "b", 4, true);
 
-            dto.Description = _lookup.FindHtmlElementByClass(html, "fullview-profile", true);
-
-            return dto;
+            _logger.LogInformation("Instance filled with analytic data");
         }
 
+        /// <summary>
+        /// Check if html content containts given symbol (stonk successfuly fetched?)
+        /// </summary>
+        /// <param name="html"></param>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
         private bool ContainsTickerId(string html, string symbol)
         {
             if (html.Contains(symbol, System.StringComparison.OrdinalIgnoreCase))
